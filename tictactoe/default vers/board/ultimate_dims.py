5eb4555f0c31d00d2010edf14406c5096e203a8d
jessica <jessicadeniran08@gmail.com>
2026-03-06 16:20:07 +0000

    Update ultimate_dims.py

--- tictactoe/default vers/board/ultimate_dims.py
+++ tictactoe/default vers/board/ultimate_dims.py
@@ -4,6 +4,7 @@ from board.const import *
 class Dimensions:
     def __init__(self,size,xcoord,ycoord):
         self.size=size # screen size
-        self.squaresize=size//BOARD_ROWS # size of each square within boatf
+        self.squaresize=size//BOARD_ROWS # size of each square within board
         self.x=xcoord # x/y coord of square
         self.y=ycoord
+
